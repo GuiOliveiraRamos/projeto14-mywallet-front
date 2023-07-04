cb676a354ead0402f7edf240a1b97c6318cb6c36
GuiOliveiraRamos <guioliveiraramos30@gmail>
2023-07-04 09:14:56 -0300

    login function

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,22 +1,53 @@
-import styled from "styled-components"
-import { Link } from "react-router-dom"
-import MyWalletLogo from "../components/MyWalletLogo"
+import styled from "styled-components";
+import { Link, useNavigate } from "react-router-dom";
+import MyWalletLogo from "../components/MyWalletLogo";
 
 export default function SignInPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const signInUp = (e) => {
+    e.preventDefault();
+    const saveData = {
+      userEmail: email,
+      userPassword: password,
+    };
+    const request = fetch("http://localhost:5173/", {
+      email,
+      password,
+    });
+    request.then(() => {
+      navigate("/home", { state: { data: Response.data } });
+    });
+    request.catch(() => {
+      window.location.reload("erro ao cadastrar usuário, tente novamente");
+    });
+  };
+
   return (
     <SingInContainer>
-      <form>
+      <form onSubmit={signInUp}>
         <MyWalletLogo />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <button>Entrar</button>
+        <input
+          placeholder="E-mail"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          placeholder="Senha"
+          type="password"
+          autocomplete="new-password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Entrar</button>
       </form>
 
-      <Link to="/cadastro">
-        Primeira vez? Cadastre-se!
-      </Link>
+      <Link to="/cadastro">Primeira vez? Cadastre-se!</Link>
     </SingInContainer>
-  )
+  );
 }
 
 const SingInContainer = styled.section`
@@ -25,4 +56,4 @@ const SingInContainer = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`
+`;
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,44 +1,78 @@
-import { Link, useNavigate } from "react-router-dom"
-import styled from "styled-components"
-import MyWalletLogo from "../components/MyWalletLogo"
-import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import MyWalletLogo from "../components/MyWalletLogo";
+import { useState } from "react";
 
 export default function SignUpPage() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const navigate = useNavigate();
 
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")  
-  const [confirmPassword, setConfirmPassword] = useState("")
-  const navigate = useNavigate()
-  
   const signInUp = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const saveData = {
       userName: name,
       userEmail: email,
       userPassword: password,
-      userConfirmPassword: confirmPassword
+      userConfirmPassword: confirmPassword,
+    };
+    if (password !== confirmPassword) {
+      return alert("as senhas devem ser iguais");
     }
-  navigate("/")
-    console.log(saveData)
-  }
+    const request = fetch("http://localhost:5173/", {
+      name,
+      email,
+      password,
+      confirmPassword,
+    });
+    request.then(() => {
+      navigate("/", { state: { data: saveData } });
+    });
+    request.catch(() => {
+      window.location.reload("erro ao cadastrar usuário, tente novamente");
+    });
+
+    console.log(saveData);
+  };
 
   return (
     <SingUpContainer>
-      <form>
+      <form onSubmit={signInUp}>
         <MyWalletLogo />
-        <input placeholder="Nome" type="text" value={name} onChange={(e) => setName(e.target.value)}  />
-        <input placeholder="E-mail" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        <input placeholder="Senha" type="password" autocomplete="new-password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        <input placeholder="Confirme a senha" type="password" autoComplete="new-password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
-        <button onClick={signInUp} >Cadastrar</button>
+        <input
+          placeholder="Nome"
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          placeholder="E-mail"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          placeholder="Senha"
+          type="password"
+          autocomplete="new-password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          placeholder="Confirme a senha"
+          type="password"
+          autoComplete="new-password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        <button type="submit">Cadastrar</button>
       </form>
 
-      <Link to="/">
-        Já tem uma conta? Entre agora!
-      </Link>
+      <Link to="/">Já tem uma conta? Entre agora!</Link>
     </SingUpContainer>
-  )
+  );
 }
 
 const SingUpContainer = styled.section`
@@ -47,4 +81,4 @@ const SingUpContainer = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`
+`;
